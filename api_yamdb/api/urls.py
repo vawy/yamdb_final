e36e7e0ef37839b8e2a15af2ec3845452489edb2
Vawy <vasilyvig@gmail.com>
2022-11-07 22:25:33 +0300

    Fix 21

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import include, path
-from rest_framework import routers
 
+from rest_framework import routers
 from views import (CategoryViewSet, CommentViewSet, GenreViewSet,
                    ReviewViewSet, TitleViewSet)
 

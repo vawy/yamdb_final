4fc9429b98fd776ece61ea68add3d64ee78299c5
Vawy <vasilyvig@gmail.com>
2022-11-09 01:04:14 +0300

    f234

--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -2,11 +2,8 @@ from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
 
-from views import index
-
 
 urlpatterns = [
-    path('', index, name='home'),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('redoc/',
